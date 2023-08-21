8f0c8103fe269dd7b804daedd5f892d1a3aced75
Siyabonga Zunguza <69678930+Bonga50@users.noreply.github.com>
2023-08-21 09:37:40 +0000

    added constraints

---
+++ Generics/GenericConstraints/ConstraintListRunner.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics.GenericConstraints
+{
+    public class ConstraintListRunner
+    {
+        public static void Main(string[] args)
+        {
+            MyLinkedList<Student> myStudents = new MyLinkedList<Student>();
+
+            myStudents.AddToFirst(new Student(1,"Benn"));
+            myStudents.AddToFirst(new Student(2, "Henry"));
+            myStudents.AddToFirst(new Student(3, "Dan"));
+            myStudents.AddToFirst(new Student(4, "Stan"));
+            myStudents.AddToFirst(new Student(5, "Car"));
+
+            foreach (var student in myStudents)
+            {
+                Console.WriteLine($"My name is {student.Name} and my student id {student.StudentId}");
+            }
+
+            Console.Read();
+
+
+        }
+
+    }
+}
---
+++ Generics/GenericConstraints/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics.GenericConstraints
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string Name { get; set; }
+
+        public Student(int studentId, string name)
+        {
+            StudentId = studentId;
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return ($"{Name} - {StudentId}");
+        }
+    }
+}
